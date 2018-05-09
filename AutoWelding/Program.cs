3c186bfc28c928c82aa4451a8f9a99a84146d720
StephenChow123 <zwlps@163.com>
2018-05-09 11:55:27 +0800

    t2

--- AutoWelding/Program.cs
+++ AutoWelding/Program.cs
@@ -7,7 +7,7 @@ namespace AutoWelding
     static class Program
     {
         /// <summary>
-        /// 应用程序的主入口点。
+        /// 应用程序的主入口点。 
         /// </summary>
         [STAThread]
         static void Main()
